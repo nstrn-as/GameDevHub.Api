3c397e4ba4ae36162514893320aed4aea4eedab1
Nastaran_Asadi <asadi.nastaran.t@gmail.com>
2026-07-19 22:46:20 +0330

    Add CreateProjectRequest DTO

---
+++ GameDevHub.Api/DTOs/CreateProjectRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace GameDevHub.Api.DTOs
+{
+    public class CreateProjectRequest
+    {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Engine { get; set; } = "";
+        public string Genre { get; set; } = "";
+    }
+}
