6750f9bb95643e87631fd463c95b976f3131aca0
Nastaran_Asadi <asadi.nastaran.t@gmail.com>
2026-07-19 21:54:52 +0330

    Add project files.

---
+++ GameDevHub.Api/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿ namespace GameDevHub.Api.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Engine { get; set; } = "";
+        public string Genre { get; set; } = "";
+        public ProjectStatus Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
---
+++ GameDevHub.Api/Models/ProjectStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace GameDevHub.Api.Models
+{
+    public enum ProjectStatus
+    {
+        planning,
+        InProgress,
+        OnHold,
+        Completed
+    }
+}
