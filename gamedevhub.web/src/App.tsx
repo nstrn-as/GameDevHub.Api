c0d719fac5c79f194a9ea0deb836f9f77348b0e8
Nastaran_Asadi <asadi.nastaran.t@gmail.com>
2026-08-22 10:32:31 +0330

    Add React frontend project

---
+++ gamedevhub.web/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+    return (
+        <div>
+            <h1>GameDevHub</h1>
+
+            <p>Your game development workspace</p>
+
+            <ul>
+                <li>Projects</li>
+                <li>Tasks</li>
+                <li>Design</li>
+                <li>Art</li>
+                <li>Music</li>
+            </ul>
+        </div>
+    );
+}
+
+export default App;
